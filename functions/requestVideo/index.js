4f2a95fdf4e6aa150a26eeb29b2c8742fccdaf83
Aaron Hooper <rnjckhpr@gmail.com>
2023-02-02 15:48:23 +0000

    Scaffold basic functionality for requestVideo

--- functions/requestVideo/index.js
+++ functions/requestVideo/index.js
@@ -1,8 +1,31 @@
+import { v4 as uuidv4 } from 'uuid'
+
 export const handler = async (event) => {
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!')
+  if (!event.url) {
+    const response = {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Request must contain a url property'
+      })
+    }
+
+    return response
   }
+
+  const jobId = uuidv4()
+
+  // TODO validate url
+  // TODO connect to dynamodb
+  // TODO insert entry
+  // TODO invoke siphon lambda
+
+  const response = {
+    statusCode: 202,
+    body: JSON.stringify({
+      message: 'Job added to queue',
+      jobId
+    })
+  }
+
   return response
 }
