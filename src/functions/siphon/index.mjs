03fe85e68189ec4410c0d7c3c47016316486844e
Aaron Hooper <rnjckhpr@gmail.com>
2023-02-15 16:13:30 +0000

    Write siphon handler

---
+++ src/functions/siphon/index.mjs
@@ -0,0 +1,46 @@
+import { Upload } from '@aws-sdk/lib-storage'
+import { PassThrough } from 'stream'
+import { UpdateItemCommand } from '@aws-sdk/client-dynamodb'
+import ytdl from 'ytdl-core'
+import s3Client from './lib/s3Client.mjs'
+import ddbClient from './lib/dynamoDbClient.mjs'
+import createS3Url from './util/createS3Url.mjs'
+
+export const handler = async (event) => {
+  const url = event.url
+  const jobId = event.jobId
+  const videoFilename = jobId + '.mp4'
+  const passThrough = new PassThrough()
+
+  const upload = new Upload({
+    client: s3Client,
+    params: {
+      Bucket: process.env.BUCKET,
+      Key: videoFilename,
+      Body: passThrough
+    }
+  })
+
+  ytdl(url, { filter: 'audioandvideo' }).pipe(passThrough)
+  await upload.done()
+
+  await ddbClient.send(new UpdateItemCommand({
+    TableName: process.env.TABLENAME,
+    Key: { jobId },
+    UpdateExpression: 'SET status = :status, downloadLink = :downloadLink',
+    ExpressionAttributeValues: {
+      ':status': 'COMPLETED',
+      ':downloadLink': createS3Url(videoFilename)
+    },
+    ReturnValues: 'UPDATED_NEW'
+  }))
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Done!'
+    })
+  }
+
+  return response
+}
