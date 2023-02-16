3e1cd331076938cf23fee41f3f70ab9392fe453c
Aaron Hooper <rnjckhpr@gmail.com>
2023-02-16 14:08:28 +0000

    Fix policy and role mishaps

--- src/terraform/siphon.tf
+++ src/terraform/siphon.tf
@@ -15,7 +15,7 @@ resource "aws_iam_role" "siphon_role" {
 }
 
 resource "aws_iam_policy" "siphon_policy" {
-  name = "requestVideo_policy"
+  name = "siphon_policy"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -45,8 +45,8 @@ resource "aws_iam_policy" "siphon_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "attach_siphon_policy_to_role" {
-  role = aws_iam_role.siphon.name
-  policy_arn = aws_iam_policy.siphon.arn
+  role = aws_iam_role.siphon_role.name
+  policy_arn = aws_iam_policy.siphon_policy.arn
 }
 
 data "archive_file" "zip_siphon" {
@@ -58,7 +58,7 @@ data "archive_file" "zip_siphon" {
 resource "aws_lambda_function" "terraform_siphon_func" {
   filename = "${path.module}/../functions/siphon.zip"
   function_name = "siphon"
-  role = aws_iam_role.siphon.arn
+  role = aws_iam_role.siphon_role.arn
   handler = "index.handler"
   runtime = "nodejs18.x"
   depends_on = [aws_iam_role_policy_attachment.attach_siphon_policy_to_role]
