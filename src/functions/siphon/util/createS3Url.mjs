7d52a1d788e7d3b47a2269621c7b8dc73f92eb59
Aaron Hooper <rnjckhpr@gmail.com>
2023-02-18 10:44:48 +0000

    Remove files no longer used

--- src/functions/siphon/util/createS3Url.mjs
+++
@@ -1,4 +0,0 @@
-export default function createS3Url (filename) {
-  return 'https://' + process.env.BUCKET + '.s3.' + process.env.REGION +
-        '.amazonaws.com/' + encodeURIComponent(filename)
-}
