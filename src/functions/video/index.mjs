00e45e1afa534268531c78ba845bf67a5d419b0c
Aaron Hooper <rnjckhpr@gmail.com>
2023-02-19 13:08:15 +0000

    Create functions for HTTP methods

--- src/functions/video/index.mjs
+++ src/functions/video/index.mjs
@@ -1,84 +1,16 @@
-import { nanoid } from 'nanoid'
-import { isValidUrl } from './lib/util.mjs'
-import ddbClient from './lib/dynamoDbClient.mjs'
-import { PutItemCommand } from '@aws-sdk/client-dynamodb'
-import lambdaClient from './lib/lambdaClient.mjs'
-import { InvokeCommand } from '@aws-sdk/client-lambda'
+import { getVideo, postVideo } from './lib/methods.mjs'
 
 export const handler = async (event) => {
-  const json = JSON.parse(event.body)
-
-  if (!json.url) {
-    const response = {
-      statusCode: 400,
-      body: JSON.stringify({
-        message: 'Request must contain a url property'
-      })
-    }
-
-    return response
-  }
-
-  const url = json.url
-  const jobId = nanoid()
-
-  if (!isValidUrl(url)) {
-    const response = {
-      statusCode: 400,
-      body: JSON.stringify({
-        message: 'Url is not valid'
-      })
-    }
-
-    return response
-  }
-
-  try {
-    await ddbClient.send(new PutItemCommand({
-      TableName: process.env.TABLE_NAME,
-      Item: {
-        jobId: { S: jobId },
-        status: { S: 'IN_PROGRESS' },
-        sourceUrl: { S: url }
-      }
-    }))
-  } catch (err) {
-    console.error(err)
-
-    const response = {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: 'Could not create item in database'
-      })
-    }
-
-    return response
-  }
-
-  try {
-    await lambdaClient.send(new InvokeCommand({
-      InvocationType: 'Event',
-      FunctionName: process.env.FUNCTION_ARN,
-      Payload: JSON.stringify({ url, jobId })
-    }))
-  } catch (err) {
-    console.error(err)
-
-    const response = {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: 'Could not invoke siphon'
-      })
-    }
-
-    return response
+  if (event.requestContext.http.method === 'GET') {
+    return getVideo(event)
+  } else if (event.requestContext.http.method === 'POST') {
+    return postVideo(event)
   }
 
   const response = {
-    statusCode: 202,
+    statusCode: 400,
     body: JSON.stringify({
-      message: 'Job added to queue',
-      jobId
+      message: `Unsupported method '${event.http.method}'`
     })
   }
 
---
+++ src/functions/video/lib/methods.mjs
@@ -0,0 +1,90 @@
+import { nanoid } from 'nanoid'
+import { isValidUrl } from './util.mjs'
+import ddbClient from './dynamoDbClient.mjs'
+import { PutItemCommand } from '@aws-sdk/client-dynamodb'
+import lambdaClient from './lambdaClient.mjs'
+import { InvokeCommand } from '@aws-sdk/client-lambda'
+
+export const getVideo = async (event) => {
+  throw new Error('Not implemented')
+}
+
+export const postVideo = async (event) => {
+  const json = JSON.parse(event.body)
+
+  if (!json.url) {
+    const response = {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Request must contain a url property'
+      })
+    }
+
+    return response
+  }
+
+  const url = json.url
+  const jobId = nanoid()
+
+  if (!isValidUrl(url)) {
+    const response = {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Url is not valid'
+      })
+    }
+
+    return response
+  }
+
+  try {
+    await ddbClient.send(new PutItemCommand({
+      TableName: process.env.TABLE_NAME,
+      Item: {
+        jobId: { S: jobId },
+        status: { S: 'IN_PROGRESS' },
+        sourceUrl: { S: url }
+      }
+    }))
+  } catch (err) {
+    console.error(err)
+
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Could not create item in database'
+      })
+    }
+
+    return response
+  }
+
+  try {
+    await lambdaClient.send(new InvokeCommand({
+      InvocationType: 'Event',
+      FunctionName: process.env.FUNCTION_ARN,
+      Payload: JSON.stringify({ url, jobId })
+    }))
+  } catch (err) {
+    console.error(err)
+
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Could not invoke siphon'
+      })
+    }
+
+    return response
+  }
+
+  const response = {
+    statusCode: 202,
+    body: JSON.stringify({
+      message: 'Job added to queue',
+      jobId
+    })
+  }
+
+  return response
+}
