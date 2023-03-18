0cbe420f5bcb3f80af404254bf7d31e60de85b62
Aaron Hooper <rnjckhpr@gmail.com>
2023-03-18 15:45:35 +0000

    Make abstractions

--- functions/siphon/index.mjs
+++ functions/siphon/index.mjs
@@ -1,65 +1,33 @@
-import { Upload } from '@aws-sdk/lib-storage'
-import { PassThrough } from 'stream'
-import { UpdateItemCommand } from '@aws-sdk/client-dynamodb'
-import ytdl from 'ytdl-core'
-import s3Client from './lib/s3Client.mjs'
-import ddbClient from './lib/dynamoDbClient.mjs'
-import { GetObjectCommand } from '@aws-sdk/client-s3'
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
-import { createResponseObject as response } from './lib/util.mjs'
+import {
+  createResponseObject as response,
+  pipeVideoToS3Bucket,
+  generateS3Url,
+  appendDownloadLinkToJob
+} from './lib/util.mjs'
 
 export const handler = async (event) => {
   const url = event.url
   const jobId = event.jobId
   const videoFilename = jobId + '.mp4'
-  const passThrough = new PassThrough()
+  const oneHourInSecs = 3600
   let signedUrl = ''
 
   try {
-    const upload = new Upload({
-      client: s3Client,
-      params: {
-        Bucket: process.env.BUCKET,
-        Key: videoFilename,
-        Body: passThrough
-      }
-    })
-
-    ytdl(url, { filter: 'audioandvideo' }).pipe(passThrough)
-    await upload.done()
+    await pipeVideoToS3Bucket(url, videoFilename)
   } catch (err) {
     console.error(err)
     return response({ message: 'Could not stream video data to bucket' }, 500)
   }
 
   try {
-    const getVideoCommand = new GetObjectCommand({
-      Bucket: process.env.BUCKET,
-      Key: videoFilename
-    })
-
-    signedUrl = await getSignedUrl(s3Client, getVideoCommand, {
-      expiresIn: 3600
-    })
+    signedUrl = await generateS3Url(videoFilename, oneHourInSecs)
   } catch (err) {
     console.error(err)
     return response({ message: 'Could not create presigned url' }, 500)
   }
 
   try {
-    await ddbClient.send(new UpdateItemCommand({
-      TableName: process.env.TABLE_NAME,
-      Key: { jobId: { S: jobId } },
-      UpdateExpression: 'SET #status = :status, downloadLink = :downloadLink',
-      ExpressionAttributeNames: {
-        '#status': 'status'
-      },
-      ExpressionAttributeValues: {
-        ':status': { S: 'COMPLETED' },
-        ':downloadLink': { S: signedUrl }
-      },
-      ReturnValues: 'UPDATED_NEW'
-    }))
+    await appendDownloadLinkToJob(jobId, signedUrl)
   } catch (err) {
     console.error(err)
     return response({ message: 'Could not update item in database' }, 500)
--- functions/siphon/lib/util.mjs
+++ functions/siphon/lib/util.mjs
@@ -1,6 +1,59 @@
+import { PassThrough } from 'stream'
+import { Upload } from '@aws-sdk/lib-storage'
+import ytdl from 'ytdl-core'
+import { GetObjectCommand } from '@aws-sdk/client-s3'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import { UpdateItemCommand } from '@aws-sdk/client-dynamodb'
+import ddbClient from './lib/dynamoDbClient.mjs'
+import s3Client from './s3Client.mjs'
+
 export function createResponseObject (object, statusCode = 200) {
   return {
     statusCode,
     body: JSON.stringify(object)
   }
+}
+
+export function pipeVideoToS3Bucket (url, filename) {
+  const passThrough = new PassThrough()
+  const upload = new Upload({
+    client: s3Client,
+    params: {
+      Bucket: process.env.BUCKET,
+      Key: filename,
+      Body: passThrough
+    }
+  })
+
+  ytdl(url, { filter: 'audioandvideo' }).pipe(passThrough)
+  return upload.done()
+}
+
+export function generateS3Url (filename, expiresIn) {
+  const getObjectCommand = new GetObjectCommand({
+    Bucket: process.env.BUCKET,
+    Key: filename
+  })
+
+  return getSignedUrl(s3Client, getObjectCommand, { expiresIn })
+}
+
+export function appendDownloadLinkToJob (jobId, downloadLink) {
+  return ddbClient.send(new UpdateItemCommand({
+    TableName: process.env.TABLE_NAME,
+    Key: { jobId: { S: jobId } },
+
+    // 'status' is a magic word in DynamoDB, so we have to use a
+    // name attribute to tell it that we are talking about our
+    // field.
+    UpdateExpression: 'SET #status = :status, downloadLink = :downloadLink',
+    ExpressionAttributeNames: {
+      '#status': 'status'
+    },
+    ExpressionAttributeValues: {
+      ':status': { S: 'COMPLETED' },
+      ':downloadLink': { S: downloadLink }
+    },
+    ReturnValues: 'UPDATED_NEW'
+  }))
 }
